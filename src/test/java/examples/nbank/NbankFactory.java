34b81f92a61e52c8f44571a7857e92a9f40f8768
gtrofimov <gttrofimov@parasoft.com>
2020-08-07 00:02:36 -0700

    fix factory

--- src/test/java/examples/nbank/NbankFactory.java
+++ src/test/java/examples/nbank/NbankFactory.java
@@ -16,11 +16,11 @@ public class NbankFactory {
 	}
 	public static CreditCard createCreditCard() {
 		int balance = 1000; // UTA: default value
-		String id = "12345"; // UTA: default value
-		String securityNumber = "123"; // UTA: default value
+		String id = "123-45-6789"; // UTA: default value
+		String securityNumber = "123-45-6789"; // UTA: default value
 		String name = "john"; // UTA: default value
 		String zipcode = "91016"; // UTA: default value
-		String creditCardNumber = "4111111111111111"; // UTA: default value
+		String creditCardNumber = "4111-1111-1111-1111"; // UTA: default value
 		return new CreditCard(balance, id, securityNumber, name, zipcode, creditCardNumber);
 	}
 
